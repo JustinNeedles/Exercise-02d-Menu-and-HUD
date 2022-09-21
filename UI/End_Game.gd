12251a534ae64476b80e3319e99693fa96d3cf43
haycon0 <32018882+haycon0@users.noreply.github.com>
2022-09-21 01:48:00 -0400

    I Dislike Forward

---
+++ UI/End_Game.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = "Thanks for playing! Your final score was " + str(Global.score) + "."
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Play_again_pressed():
+	Global.reset()
+	var _scene = get_tree().change_scene("res://Game.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
